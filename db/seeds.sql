ea0315c3e0f8ad23e2920e4dccf53697513ba4ff
Nick Macon <nickmacon@gmail.com>
2020-01-21 20:58:34 -0600

    1.21 20:58 push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('fajita chicken taco');
+INSERT INTO burgers (name) VALUES ('chorizo, egg, & cheese taco');
+INSERT INTO burgers (name, devoured) VALUES ('carne guisada taco', true);
+INSERT INTO burgers (name, devoured) VALUES ('tacos al carbon', true);
+INSERT INTO burgers (name, devoured) VALUES ('fish taco', true);
+INSERT INTO burgers (name) VALUES ('barbacoa taco');
